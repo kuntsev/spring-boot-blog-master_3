03dd66b7176f1a0bb58dba5ef7d484ec71802c2d
kuntsev <kuntsev@ukr.net>
2023-04-26 18:07:38 +0300

    added test on cucumber step defs

--- src/main/java/LoginStepdefs.java
+++ src/main/java/LoginStepdefs.java
@@ -1,18 +1,26 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginStepdefs {
-    public LoginStepdefs() {
-        Given("^I'm on \"([^\"]*)\" page$", (String arg0) -> {
-        });
-        And("^fill user name with \"([^\"]*)\"$", (String arg0) -> {
-        });
-        And("^fill password with \"([^\"]*)\"$", (String arg0) -> {
-        });
-        When("^I click on \"([^\"]*)\" button$", (String arg0) -> {
-        });
-        Then("^I should be loggined successfully$", () -> {
-        });
-        And("^I should be redirected on \"([^\"]*)\" page$", (String arg0) -> {
-        });
-        And("^be happy$", () -> {
-        });
+    @Given("I'm on {} page")
+    public void iMOnPage(String arg0) {
+    }
+
+    @And("fill {string} with {}")
+    public void fillWith(String arg0, String arg1) {
+    }
+
+    @And("I fill {string} with {}")
+    public void iFillWith(String arg0, String arg1) {
+    }
+
+    @When("I click on {string} button")
+    public void iClickOnButton(String arg0) {
+    }
+
+    @Then("I should be redirected on {} page")
+    public void iShouldBeRedirectedOnPage(String arg0) {
     }
 }
--- src/main/resources/features/login.feature
+++ src/main/resources/features/login.feature
@@ -1,12 +1,13 @@
-language #en
 Feature: user can login to Demo app with specific credentials
 
   Scenario Outline: Successful login
-    Given I'm on "login" page
-    And fill user name with "user"
-    And fill password with "password"
+    Given I'm on <login> page
+    And fill "user name" with <user>
+    And  I fill "password" with <password>
     When I click on "Login" button
-    Then I should be loggined successfully
-    And I should be redirected on "home" page
-    And be happy
+    Then I should be redirected on <home> page
+
+
     Examples:
+      | login                       | user | password | home                       |  |
+      | http://localhost:8090/login | user | password | http://localhost:8090/home |  |
